ab08bd6b79b8abda874c3ac93bbd848afcd13b5f
samuel123 <smaylopco@yahoo.es>
2016-11-04 09:03:54 -0600

    se arreglaron errores

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class contactForm(forms.Form):
+        Email     = forms.EmailField(widget=forms.TextInput())
+        Titulo    = forms.CharField(widget=forms.TextInput())
+        Texto     = forms.CharField(widget=forms.Textarea())
+
+class LoginForm(forms.Form):
+        username    = forms.CharField(widget=forms.TextInput())
+        password    = forms.CharField(widget=forms.PasswordInput(render_value=False))
+    
